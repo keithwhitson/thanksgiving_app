82777d800673d601cfd1472035bf897a344ed337
Keith Whitson <keith.whitson@franka.de>
2021-11-03 16:05:14 +0100

    added

--- src/Recipes.js
+++ src/Recipes.js
@@ -9,14 +9,27 @@ export const Recipes = () => {
           {recipeInfo.map((data, key) => {
             return (
               <div key={key}>
-                <img src={data.image} width="700" height="700" />
-                <p><b>Label: </b></p>
-                <p>{data.label}</p>
-                <p><b>Source: </b></p>
-                <p>{data.source}</p>
-                <p><b>Calories: </b></p>
-                <p>{data.calories}</p>
-                <div classNme="dishtype-container">
+
+                <div className="image">
+                    <img src={data.image} width="700" height="700" />
+                </div>
+                
+                <div className="label">
+                    <p><b>Label: </b></p>
+                    <p>{data.label}</p>
+                </div>
+                
+                <div className="source">
+                    <p><b>Source: </b></p>
+                    <p>{data.source}</p>
+                </div>
+                
+                <div className="calories">
+                    <p><b>Calories: </b></p>
+                    <p>{data.calories}</p>
+                </div>
+ 
+                <div className="dishtype-container">
                 <p><b>DishType: </b></p>
                 {data.dishType.map(item => {
                     return <p>{item}</p>;
