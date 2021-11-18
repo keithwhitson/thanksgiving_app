0e7ac8e537e13cebc9b7eeadb64ad9e93963809f
keithwhitson <keith_whitson@yahoo.com>
2021-11-18 01:10:59 +0100

    was able to add the functionality to toggle the view

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,35 +1,40 @@
-import Reach from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const NavBar = () => (
+const NavBar = () => {
+    const [showLinks, setShowLinks] = useState(false);
+    return(
     <>
     <div className="header-container">
     <nav className="NavbarItems"> 
     <div  id="">    
-        <ul class="linked-list">
-            <li className="">
+        <ul class="linked-list" id={showLinks ? "hidden" : ""}>
+            <li className="links">
                 <Link to="/" className="">Home</Link>
             </li>
-            <li className="">
+            <li className="links">
                 <Link to="/starters" className="">Starters</Link>
             </li>
 
-            <li className="">
+            <li className="links">
                 <Link to="/desserts" className="">Desserts</Link>
             </li>
-            <li className="">
+            <li className="links">
                 <Link to="/maincourse" className="">Main Courses</Link>
             </li>
 
-            <li className="">
+            <li className="links">
                 <Link to="/drinks" className="">Drinks</Link>
+            </li> 
+            <li className="">
+                <button onClick={() => setShowLinks(!showLinks)}>Open</button>
             </li> 
         </ul>
         </div>
     </nav>
     </div>
-    </>
+    </>);
 
-);
+    };
 
 export default NavBar;
